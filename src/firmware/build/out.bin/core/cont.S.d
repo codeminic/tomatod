b087c50676832609af0759660d771407c409f7a0
Dominic Sauder <dominic.sauder@gmail.com>
2022-06-11 14:38:55 +0200

    ignore build output

--- src/firmware/build/out.bin/core/cont.S.d
+++
@@ -1,2 +0,0 @@
-d:\Projects\tomato-guard-3000\src\firmware\build\out.bin\core\cont.S.o: \
- C:\Users\Dominic\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\2.6.3\cores\esp8266\cont.S
--- src/firmware/build/out.bin/core/crc32.cpp.d
+++
@@ -1,8 +0,0 @@
-d:\Projects\tomato-guard-3000\src\firmware\build\out.bin\core\crc32.cpp.o: \
- C:\Users\Dominic\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\2.6.3\cores\esp8266\crc32.cpp \
- C:\Users\Dominic\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\2.6.3\cores\esp8266\coredecls.h \
- C:\Users\Dominic\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\2.6.3/tools/sdk/libc/xtensa-lx106-elf/include/stdint.h \
- C:\Users\Dominic\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\2.6.3/tools/sdk/libc/xtensa-lx106-elf/include/machine/_default_types.h \
- C:\Users\Dominic\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\2.6.3/tools/sdk/libc/xtensa-lx106-elf/include/sys/features.h \
- C:\Users\Dominic\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\2.6.3/tools/sdk/libc/xtensa-lx106-elf/include/sys/_intsup.h \
- C:\Users\Dominic\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\2.6.3\cores\esp8266/cont.h
