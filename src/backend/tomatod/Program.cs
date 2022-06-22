0d9860b3dbbd75dcb75aa85667dfce0c44aae78a
Dominic Sauder <dominic.sauder@gmail.com>
2022-06-22 20:38:29 +0200

    add vue client app

--- src/backend/tomatod/Program.cs
+++ src/backend/tomatod/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Identity.Web;
 using tomatod;
 
@@ -18,30 +19,31 @@ builder.Services.AddTransient<Greenhouse>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+app.UseStaticFiles(new StaticFileOptions { FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/dist"))});
 
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.MapFallbackToFile("dist/index.html");
 
 app.MapGet("/status", (HttpContext httpContext) =>
 {
 })
 .WithName("Get status");
 
-app.MapPost("/greenhouse/water", async ([FromServices] Greenhouse greenhouse) => await greenhouse.WaterPlants())
+app.MapPost("/api/greenhouse/water", async ([FromServices] Greenhouse greenhouse) => await greenhouse.WaterPlants())
 .WithName("Water plants");
 
-app.MapPost("/greenhouse/shutter/open", async ([FromServices] Greenhouse greenhouse) => await greenhouse.OpenShutter())
+app.MapPost("/api/greenhouse/shutter/open", async ([FromServices] Greenhouse greenhouse) => await greenhouse.OpenShutter())
 .WithName("Open shutter");
 
-app.MapPost("/greenhouse/shutter/close", async ([FromServices] Greenhouse greenhouse) => await greenhouse.CloseShutter())
+app.MapPost("/api/greenhouse/shutter/close", async ([FromServices] Greenhouse greenhouse) => await greenhouse.CloseShutter())
 .WithName("Close shutter");
 
 app.Run();
 
+app.MapFallbackToFile("index.html");
---
+++ src/backend/tomatod/wwwroot/src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button @click="openShutter">Open shutter</button>
+    <button @click="closeShutter">Close shutter</button>
+    <button @click="waterPlants">Water plants</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  methods: {
+    openShutter: async function () {
+      await fetch("/api/greenhouse/shutter/open", { method: "POST" });
+    },
+    closeShutter: async function () {
+      await fetch("/api/greenhouse/shutter/close", { method: "POST" });
+    },
+    waterPlants: async function () {
+      await fetch("/api/greenhouse/water", { method: "POST" });
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
